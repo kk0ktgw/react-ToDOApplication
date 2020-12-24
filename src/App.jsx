bef20e3cbd26f9f9e4544f741ccca52a1210e9ef
kk0ktgw <76461553+kk0ktgw@users.noreply.github.com>
2020-12-24 15:13:26 +0900

    ToDoApplication

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./styles.css";
 import { InputTodo } from "./components/InputTodo";
+import { IncompleteTodos } from "./components/IncompleteTodos";
+import { CompleteTodos } from "./components/CompleteTodos";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
@@ -44,35 +46,19 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteToDos.length >= 5}
       />
-      <div className="imcompleteArea">
-        <p className="title">未完了のToDo</p>
-        <ul>
-          {incompleteToDos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickComplete(index)}>完了</button>
-                <button onClick={() => onClickDelete(index)}>削除</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
-
-      <div className="completeArea">
-        <p className="title">完了したToDo</p>
-        <ul>
-          {completeToDos.map((todo, index) => {
-            return (
-              <div key={todo} className="list-row">
-                <li>{todo}</li>
-                <button onClick={() => onClickBack(index)}>戻す</button>
-              </div>
-            );
-          })}
-        </ul>
-      </div>
+      {incompleteToDos.length >= 5 && (
+        <p style={{ color: "red" }}>
+          5つ以上入力できません。まずはToDoを消化しましょう。
+        </p>
+      )}
+      <IncompleteTodos
+        todos={incompleteToDos}
+        onClickComplete={onClickComplete}
+        onClickDelete={onClickDelete}
+      />
+      <CompleteTodos todos={completeToDos} onClickBack={onClickBack} />
     </>
   );
 };
--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 
+const style = {
+  backgroundColor: "aquamarine",
+  width: "400px",
+  height: "30px",
+  padding: "8px",
+  margin: "8px",
+  borderRadius: "7px"
+};
+
+const button = {
+  borderRadius: "6px",
+  border: "none",
+  padding: "6px 10px",
+  marginLeft: "5px"
+};
+
+const input = {
+  borderRadius: "6px",
+  border: "none",
+  padding: "6px 10px",
+  outline: "none"
+};
+
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <>
-      <div className="inputArea">
-        <input placeholder="ToDOを入力" value={todoText} onChange={onChange} />
-        <button onClick={onClick}>追加</button>
+      <div style={style}>
+        <input
+          disabled={disabled}
+          placeholder="ToDOを入力"
+          value={todoText}
+          onChange={onChange}
+          style={input}
+        />
+        <button style={button} disabled={disabled} onClick={onClick}>
+          追加
+        </button>
       </div>
     </>
   );
